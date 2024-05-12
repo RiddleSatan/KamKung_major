a43a5dfdc941c4c787ad38912da0b74d209385db
Om Pandey <mynameisompandey007@gmail.com>
2024-05-12 22:29:08 +0530

    somechanges

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,10 @@ import { NavLink } from "react-router-dom";
 const Home = () => {
   return (
     <div className="w-full min-h-screen  px-2">
-      <div className="w-[95%] h-[98vh] px-4 flex items-center" >
+      <div className="w-[95%] h-[98vh] px-4 flex items-center">
         <div className="title w-[40vw]    ml-12">
           <h1 className="text-6xl font-medium tracking-tighter">
-            Experience a New Era of Samsung AI TV 
+            Experience a New Era of Samsung AI TV
           </h1>
           <h1 className="font-light mt-5">Neo QLED | Neo QLED 8K | OLED</h1>
           <NavLink
@@ -17,51 +17,82 @@ const Home = () => {
             Buy now
           </NavLink>
         </div>
-        <div className="image"> <img src="https://images.samsung.com/is/image/samsung/p6pim/in/qa65qn85caklxl/gallery/in-qled-qn85c-qa65qn85caklxl-535909596?$720_576_PNG$" alt="" /></div>
+        <div className="image">
+          {" "}
+          <img
+            src="https://images.samsung.com/is/image/samsung/p6pim/in/qa65qn85caklxl/gallery/in-qled-qn85c-qa65qn85caklxl-535909596?$720_576_PNG$"
+            alt=""
+          />
+        </div>
       </div>
-       <h1 className="text-4xl font-medium w-fit mx-auto tracking-tighter mt-8">This Week's Highlights</h1>
-       <div className=" flex justify-center" > 
-       <NavLink
-            className="px-6 py-[0.60rem] rounded-[20px]  mt-8 inline-block  text-lg font-medium  transition-all hover:text-gray-700 active:underline "
-            to=""
-          >
-            New In
-          </NavLink>
-       <NavLink
-            className="px-6 py-[0.60rem] rounded-[20px]  mt-8 inline-block  text-lg font-medium  transition-all hover:text-gray-700 active:underline "
-            to=""
-          >
-           Mobile
-          </NavLink>
-       <NavLink
-            className="px-6 py-[0.60rem] rounded-[20px]  mt-8 inline-block  text-lg font-medium  transition-all hover:text-gray-700 active:underline "
-            to=""
-          >
-            TV
-          </NavLink>
-       <NavLink
-            className="px-6 py-[0.60rem] rounded-[20px]  mt-8 inline-block  text-lg font-medium  transition-all hover:text-gray-700 active:underline "
-            to=""
-          >
-            Appliances
-          </NavLink>
-       <NavLink
-            className="px-6 py-[0.60rem] rounded-[20px]  mt-8 inline-block  text-lg font-medium  transition-all hover:text-gray-700 active:underline "
-            to=""
-          >
-            Galaxy Ecosystem
-          </NavLink>
-       <NavLink
-            className="px-6 py-[0.60rem] rounded-[20px]  mt-8 inline-block  text-lg font-medium  transition-all hover:text-gray-700 active:underline "
-            to=""
-          >
-            Buy now
-          </NavLink>
-       </div>
+      <h1 className="text-4xl font-medium w-fit mx-auto tracking-tighter mt-8">
+        This Week's Highlights
+      </h1>
+      <div className=" flex justify-center">
+        <NavLink
+          className="px-6 py-[0.60rem] rounded-[20px]  mt-8 inline-block  text-lg font-medium  transition-all hover:text-gray-700 active:underline "
+          to=""
+        >
+          New In
+        </NavLink>
+        <NavLink
+          className="px-6 py-[0.60rem] rounded-[20px]  mt-8 inline-block  text-lg font-medium  transition-all hover:text-gray-700 active:underline "
+          to=""
+        >
+          Mobile
+        </NavLink>
+        <NavLink
+          className="px-6 py-[0.60rem] rounded-[20px]  mt-8 inline-block  text-lg font-medium  transition-all hover:text-gray-700 active:underline "
+          to=""
+        >
+          TV
+        </NavLink>
+        <NavLink
+          className="px-6 py-[0.60rem] rounded-[20px]  mt-8 inline-block  text-lg font-medium  transition-all hover:text-gray-700 active:underline "
+          to=""
+        >
+          Appliances
+        </NavLink>
+        <NavLink
+          className="px-6 py-[0.60rem] rounded-[20px]  mt-8 inline-block  text-lg font-medium  transition-all hover:text-gray-700 active:underline "
+          to=""
+        >
+          Galaxy Ecosystem
+        </NavLink>
+        <NavLink
+          className="px-6 py-[0.60rem] rounded-[20px]  mt-8 inline-block  text-lg font-medium  transition-all hover:text-gray-700 active:underline "
+          to=""
+        >
+          Buy now
+        </NavLink>
+      </div>
+      <div className="  rounded-2xl  bg-red-700 overflow-hidden flex">
+        <div className="overflow-hidden  w-50% ">
+          <img
+            src="https://images.samsung.com/is/image/samsung/assets/in/home/0426/home_page_bigtile-sqaure_pc.jpg?$684_684_JPG$"
+            alt=""
+          />
+        </div>
+        <div className="w-[48%]  flex flex-wrap gap-2 items-center justify-center overflow-hidden mx-auto">
+          <div className="w-[48%]  bg-cyan-400 overflow-hidden ">
+            <img className="object-cover w-full h-full" src="https://images.samsung.com/is/image/samsung/assets/in/home/RF65DB90BD12TL_Small_Tile_330x330.jpg?$330_330_JPG$" alt="" />
+          </div>
+          <div className="w-[48%]  bg-cyan-400 overflow-hidden">
+            <img className="object-cover w-full h-full" src="https://images.samsung.com/is/image/samsung/assets/in/home/RF65DB90BD12TL_Small_Tile_330x330.jpg?$330_330_JPG$" alt="" />
+          </div>
+
+          <div className="w-[48%]  bg-cyan-400 overflow-hidden">
+            <img className="object-cover w-full h-full" src="https://images.samsung.com/is/image/samsung/assets/in/home/RF65DB90BD12TL_Small_Tile_330x330.jpg?$330_330_JPG$" alt="" />
+          </div>
+
+          <div className="w-[48%]  bg-cyan-400 overflow-hidden">
+            <img className="object-cover w-full h-full" src="https://images.samsung.com/is/image/samsung/assets/in/home/RF65DB90BD12TL_Small_Tile_330x330.jpg?$330_330_JPG$" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
-
 export default Home;
 
 // Stop automatic slide show
@@ -83,3 +114,5 @@ export default Home;
 // Neo QLED | Neo QLED 8K | OLED
 //
 //
+
+// #f4f4f4
