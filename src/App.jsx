cc8a075b0cd2d7a4e7278e158e0cb73178b2a5af
Om Pandey <mynameisompandey007@gmail.com>
2024-05-12 09:25:18 +0530

    signup form designed

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import { Outlet } from 'react-router-dom'
 import Login from './components/Login'
+import SignUp from './components/SignUp'
 
 function App() {
 
   return (
     <>
     <Header/>
-    <Login/>
+    <SignUp/>
     {/* <Outlet/> */}
      <Footer/>
     </>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="w-full h-screen pl-5 flex my-4">
+      <div className="left w-[35vw] min-h-[30vh]  flex flex-col items-center justify-between py-14">
+      <h1 className="font-medium tracking-tighter text-2xl">Login</h1>
+      <h1 className="font-medium  text-lg">Login with email to become a member</h1>
+      <div className="h-auto w-fit ">
+        <form  action="/signup" method="post">
+          <label htmlFor="">Email:</label>
+          <input className="block px-10 items-center rounded-lg py-2  mb-4 outline-none border-b-[1px] border-black" type="email" name="email" placeholder="Enter your Email" />
+          <label htmlFor="">Password:</label>
+          <input className="block px-10 items-center rounded-lg py-2  mb-4 outline-none border-b-[1px] border-black"
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+          />
+          <label htmlFor="">Fullname:</label>
+          <input className="block px-10 items-center rounded-lg py-2  mb-4 outline-none border-b-[1px] border-black" type="text" name="fullname" placeholder="Enter your full Name" />
+          <label htmlFor="">Username:</label>
+          <input  className="block px-10 items-center rounded-lg py-2  mb-4 outline-none border-b-[1px] border-black"type="text" name="username" placeholder="Enter username" />
+          {/* <input type="file" name="profilepic" /> */}
+          <div className="flex flex-col justify-between items-center">
+          <button className="block w-[18vw] py-[0.35rem]  tracking-tighter rounded-3xl text-white bg-black" type="submit">Sign-Up</button>
+                           <h1 className="my-1 tracking-tighter">OR</h1>
+          <button className="block w-[18vw] py-[0.35rem] tracking-tighter  rounded-3xl text-white bg-[#2187fd] " type="submit">Log-in</button>
+          </div>
+        </form>
+      </div>
+      </div>
+      <div className="right w-fit h-auto overflow-hidden flex items-center justify-center "> 
+      <img className="w-[60vw]  object-cover rounded-md" src="https://cdn.wallpapersafari.com/59/52/FsLec6.jpg" alt="" /></div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
