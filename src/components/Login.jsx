a68aa0f0d74df708ff646bba59789119638fd121
Om Pandey <mynameisompandey007@gmail.com>
2024-05-23 08:44:52 +0530

    api configured IMP

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,10 +8,19 @@ const [email, setEmail] = useState('')
 const [password, setPassword] = useState('')
 const navigate=useNavigate()
 
+const api = axios.create({
+    baseURL: 'http://localhost:3000',
+    withCredentials: true,//indicates whether or not cross-site Access-Control requests should be made using credentials such as cookies, authorization headers
+    headers: {//This defines default HTTP headers that will be sent with every request made using this Axios instance.
+      "Content-Type": "application/json",//Specifies that the data being sent in the request body is JSON.
+      Accept: "application/json",//Indicates that the client expects a JSON response from the server.
+    },
+  });
+
 async function handleSubmit(e){
 	e.preventDefault()
     try {
-		const response= await axios.post(`http://localhost:3000/login`,{email,password})
+		const response = await api.post('/login', { email, password });
 		console.log(response.data.id)
 		if(response.data.id){
 			navigate('/',{state:{userId:response.data.id}})
