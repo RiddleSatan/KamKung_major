1550acf94e2006f3744327757df5def8c80059d7
Om Pandey <mynameisompandey007@gmail.com>
2024-05-23 08:54:38 +0530

    some changes

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -20,10 +20,19 @@ const SignUp = () => {
     }
   };
 
+  const api = axios.create({
+    baseURL: 'http://localhost:3000',
+    withCredentials: true,//indicates whether or not cross-site Access-Control requests should be made using credentials such as cookies, authorization headers
+    headers: {//This defines default HTTP headers that will be sent with every request made using this Axios instance.
+      "Content-Type": "application/json",//Specifies that the data being sent in the request body is JSON.
+      Accept: "application/json",//Indicates that the client expects a JSON response from the server.
+    },
+  });
+
   async function handlesubmit(e) {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:3000/signup", {
+      const response = await api.post("http://localhost:3000/signup", {
         email,
         password,
         fullname,
